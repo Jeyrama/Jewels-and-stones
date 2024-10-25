e4a86b491c2290b0e5363fada04d856dfb9751d4
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-10-25 13:04:02 -0700

    Update Jewels-and-stones.js

--- Jewels-and-stones.js
+++ Jewels-and-stones.js
@@ -23,6 +23,19 @@ Constraints:
 
 // Solution
 
+function findJewels(jewels, stones) {
+  const map = {};
+  let count = 0;
+  for (const jewel of jewels) {
+    map[jewel] = true
+  }
+  for (const stone of stones) {
+    if (map[stone]) {
+      count++
+    }
+  }
+  return count
+}
 
 // or
 
